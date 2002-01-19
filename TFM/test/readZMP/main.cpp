b67db7d571c614ab3dbc6e33aa025cac5029cc48
lpinel <lolipv92@hotmail.com>
2002-01-19 05:49:00 +0100

    Solved compiling error

--- TFM/test/readZMP/main.cpp
+++ TFM/test/readZMP/main.cpp
@@ -27,7 +27,7 @@ yarp::os::Port port0;
 yarp::os::Port port1;
 float fx0, fy0, fz0, mx0, my0, mz0;
 float fx1, fy1, fz1, mx1, my1, mz1;
-float e = 0.194  // distance [m] between ground and sensor center
+float e = 0.194;  // distance [m] between ground and sensor center
 float xzmp0, yzmp0; // Right foot ZMP
 float xzmp1, yzmp1; // Left foot ZMP
 float xzmp, yzmp; // Global ZMP
@@ -82,8 +82,8 @@ int main(void) {
         xzmp = (xzmp0 * fz0 + xzmp1 * fz1) / (fz0 + fz1);
         yzmp = (yzmp0 * fz0 + yzmp1 * fz1) / (fz0 + fz1);
         
-        xzmp = xzmp * 1000 //in mm
-        yzmp = yzmp * 1000
+        xzmp = xzmp * 1000; //in mm
+        yzmp = yzmp * 1000;
         
         if ((xzmp != xzmp) || (yzmp != yzmp)){
             printf ("Warning: No zmp data\n");
