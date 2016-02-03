32853ed4195ba763b0a85e314ffb192e7377fd39
lpinel <lolipv92@hotmail.com>
2016-02-03 18:25:36 +0100

    Cambiado el envio de datos por un solo puerto yarp

--- TFM/control/simulacion/lectura/main.cpp
+++ TFM/control/simulacion/lectura/main.cpp
@@ -17,21 +17,16 @@ using namespace std;
 int main(void) {
 
     yarp::os::Network yarp;
-    yarp::os::Port port0;
-    yarp::os::Port port1;
+    yarp::os::Port port;
 
-    port0.open("/jr3:o");
-    port1.open("/system:o");
-
-    yarp::os::Bottle jr3data;
-    yarp::os::Bottle bottle;
+    port.open("/jr3:o");
 
     six_axis_array fm;
     force_array fs;
     int ret, fd;
     LIPM2d modelo;
-    float xzmp = 0;
-    float yzmp = 0;
+    float xzmp = 0.0;
+    float yzmp = 0.0;
 
     if ((fd=open("/dev/jr3",O_RDWR)) < 0) {
         perror("Can't open device. No way to read force!");
@@ -43,6 +38,8 @@ int main(void) {
     ret=ioctl(fd,IOCTL0_JR3_ZEROOFFS);
 
     while (1) {
+        yarp::os::Bottle bottle;
+
         ret=ioctl(fd,IOCTL0_JR3_FILTER0,&fm);
 
         if (ret!=-1) {
@@ -53,18 +50,17 @@ int main(void) {
                 int f = 100*fm.f[i]*fs.f[i]/16384;
                 fz = 100*fm.f[2]*fs.f[2]/16384;
                 printf("%d ",f);
-                jr3data.addInt(f);
+                bottle.addInt(f);
             }
             for (int i=0;i<3;i++) {
                 int m = 10*fm.m[i]*fs.m[i]/16384;
                 mx = 10*fm.m[0]*fs.m[0]/16384;
                 my = 10*fm.m[1]*fs.m[1]/16384;
                 printf("%d ",m);
-                jr3data.addInt(m);
+                bottle.addInt(m);
             }
 
             printf("]\n");
-            port0.write(jr3data);
 
             //printf("--\n");
             //printf("%d\n",(short)fs.v[1]/16384);
@@ -73,14 +69,12 @@ int main(void) {
             fz = fz /100;
             my = my /10;
             mx = mx/10;
-            /**
-          * CALCULO ZMP
-          * ZMPx = -My/Fz
-          * ZMPy = Mx/Fz
-          **/
-            if (fz !=0){
-                xzmp = -my / fz;
-                yzmp = mx / fz;
+
+
+            // CALCULO ZMP
+            if (fz !=0.0){
+                xzmp = (float) (-my / fz);
+                yzmp = (float) (mx / fz);
             } else{
                 xzmp=0;
                 yzmp=0;
@@ -94,13 +88,14 @@ int main(void) {
             cout << "model ok" << endl;
             //modelo.sendData(port1, data);
 
-            bottle.addDouble(modelo._u);
             bottle.addDouble(modelo._x1);
             bottle.addDouble(modelo._x2);
+            bottle.addDouble(modelo._u);
             bottle.addDouble(modelo._y);
+
             cout << "data bottled" << endl;
 
-            port1.write(bottle);
+            port.write(bottle);
 
             cout << "data sent"<< endl;
 
