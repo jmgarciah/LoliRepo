186f6b50dc7870f56d9b17c709acc0d0627770b7
lpinel <lolipv92@hotmail.com>
2016-02-11 19:53:18 +0100

    Se han quitado comentarios de libreria

--- TFM/test/jr3pci1channel/jr3pci-ioctl.h
+++ TFM/test/jr3pci1channel/jr3pci-ioctl.h
@@ -5,19 +5,12 @@
 #define __JR3IOCTL__
 
 typedef struct force_array {
-	/* int f[3];
-	 int m[3];
-	 int v[2];
-*/
-
 	  int f[3];
 	  int m[3];
 	  int v[2];
 } force_array;
 
 typedef struct six_axis_array {
-	 // int f[3];
-	 // int m[3];
 	  int f[3];
 	  int m[3];
 
