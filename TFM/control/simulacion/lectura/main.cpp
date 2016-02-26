089047732bc9c9e9f1e068a2e6fb9afd6d4f095f
lpinel <lolipv92@hotmail.com>
2016-02-26 18:20:49 +0100

    changed model

--- TFM/control/simulacion/lectura/main.cpp
+++ TFM/control/simulacion/lectura/main.cpp
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <iostream>
+#include <string.h>
 
 #include <sys/ioctl.h>
 
@@ -22,12 +23,23 @@ int main(void) {
     yarp::os::Network yarp;
     yarp::os::Port port;
 
-    six_axis_array fm;
-    force_array fs;
+    six_axis_array fm0, fm1;
+    force_array fs0, fs1;
     int ret, fd;
     LIPM2d modelo;
+
+    int f00,f01,f02,m00,m01,m02; // F-T from the sensor 0 in Newton
+    int f10,f11,f12,m10,m11,m12; // F-T from the sensor 1 in Newton
+    float fx0, fy0, fz0, mx0, my0, mz0; // Scaled F-T from sensor 0 in dN*m (0.1N*m)
+    float fx1, fy1, fz1, mx1, my1, mz1; // Scaled F-T from sensor 1 in dN*m (0.1N*m)
+    float xzmp0, yzmp0; // ZMP sensor 0
+    float xzmp1, yzmp1; // ZMP sensor 1
     float xzmp = 0.0;
     float yzmp = 0.0;
+    FILE *fp;
+
+    fp = fopen("../data.csv","w+");
+
 
     /** Check yarp network**/
     printf("Checking network...\n");
@@ -46,10 +58,12 @@ int main(void) {
     }
 
     printf("Getting full scales...\n");
-    ret=ioctl(fd,IOCTL0_JR3_GET_FULL_SCALES,&fs);
-    printf("Full scales are %d %d %d %d %d %d\n",fs.f[0],fs.f[1],fs.f[2],fs.m[0],fs.m[1],fs.m[2]);
-    printf("Setting zero offset...\n");
+    ret=ioctl(fd,IOCTL0_JR3_GET_FULL_SCALES,&fs0);
+    printf("Full scales of Sensor 0 are %d %d %d %d %d %d\n",fs0.f[0],fs0.f[1],fs0.f[2],fs0.m[0],fs0.m[1],fs0.m[2]);
+    ret=ioctl(fd,IOCTL1_JR3_GET_FULL_SCALES,&fs1);
+    printf("Full scales of Sensor 1 are: %d %d %d %d %d %d\n", fs1.f[0],fs1.f[1],fs1.f[2],fs1.m[0],fs1.m[1],fs1.m[2]);
     ret=ioctl(fd,IOCTL0_JR3_ZEROOFFS);
+    ret=ioctl(fd,IOCTL1_JR3_ZEROOFFS);
 
     /** SET CONFIG **/
     Property optionsLeftLeg;
@@ -91,99 +105,140 @@ int main(void) {
     } else printf("[success] testTEO acquired robot RightArm inteface\n");
 
 
-
+    /** SET MODE **/
     printf("Set position mode Left Leg");
     posLeftLeg->setPositionMode();
     printf("Set position mode Left Leg");
     posRightLeg->setPositionMode();
 
-    printf("Right Leg positionMove(3,30)\n");
-    posRightLeg->positionMove(3, 30);
-    printf("Right Leg positionMove(4,-5)\n");
-    posRightLeg->positionMove(4,-5);
+    Time::delay(5);
+    /** SET LEFT LEG TO 0 **/
+    printf("Left Leg positionMove(0,0)\n");
+    posLeftLeg->positionMove(0, 0);
+    printf("Left Leg positionMove(1,0)\n");
+    posLeftLeg->positionMove(1, 0);
+    printf("Left Leg positionMove(2,0)\n");
+    posLeftLeg->positionMove(2, 0);
+    printf("Left Leg positionMove(3,0)\n");
+    posLeftLeg->positionMove(3, 0);
+    printf("Left Leg positionMove(4,0)\n");
+    posLeftLeg->positionMove(4, 0);
+    printf("Left Leg positionMove(5,0)\n");
+    posLeftLeg->positionMove(5, 0);
 
-    Time::delay(2);
+    /** SET RIGHT LEG TO ZERO **/
+    printf("Right Leg positionMove(0,0)\n");
+    posRightLeg->positionMove(0, 0);
+    printf("Right Leg positionMove(1,0)\n");
+    posRightLeg->positionMove(1, 0);
+    printf("Right Leg positionMove(2,0)\n");
+    posRightLeg->positionMove(2, 0);
+    printf("Right Leg positionMove(3,0)\n");
+    posRightLeg->positionMove(3, 0);
+    printf("Right Leg positionMove(4,0)\n");
+    posRightLeg->positionMove(4, 0);
+    printf("Right Leg positionMove(5,0)\n");
+    posRightLeg->positionMove(5, 0);
 
-    while (1) {
+    int ok;
+    cin >> ok; // confirmacion de empiece
+
+    int n = 1;
+    while (ok) {
         yarp::os::Bottle bottle;
 
-        ret=ioctl(fd,IOCTL0_JR3_FILTER0,&fm);
+        ret=ioctl(fd,IOCTL0_JR3_FILTER0,&fm0);
+        ret=ioctl(fd,IOCTL1_JR3_FILTER0,&fm1);
 
         if (ret!=-1) {
 
-            float fz, my, mx;
-            printf("[ ");
-            for (int i=0;i<3;i++) {
-                int f = 100*fm.f[i]*fs.f[i]/16384; // Centesimas de Newton
-                fz = 100*fm.f[2]*fs.f[2]/16384;
-                printf("%d ",f);
-                bottle.addInt(f);
+            // -------- SENSOR 0 ------------ //
+
+            f00 = 100*fm0.f[0]*fs0.f[0]/16384;
+            f01 = 100*fm0.f[1]*fs0.f[1]/16384;
+            f02 = 100*fm0.f[2]*fs0.f[2]/16384;
+            m00 = 10*fm0.m[0]*fs0.m[0]/16384;
+            m01 = 10*fm0.m[1]*fs0.m[1]/16384;
+            m02 = 10*fm0.m[2]*fs0.m[2]/16384;
+            fx0 = (float) f00/100;
+            fy0 = (float) f01/100;
+            fz0 = (float) f02/100;
+            mx0 = (float) m00/100;
+            my0 = (float) m01/100;
+            mz0 = (float) m02/100;
+
+            // -------- SENSOR 1 ------------ //
+            f10 = 100*fm1.f[0]*fs1.f[0]/16384;
+            f11 = 100*fm1.f[1]*fs1.f[1]/16384;
+            f12 = 100*fm1.f[2]*fs1.f[2]/16384;
+            m10 = 10*fm1.m[0]*fs1.m[0]/16384;
+            m11 = 10*fm1.m[1]*fs1.m[1]/16384;
+            m12 = 10*fm1.m[2]*fs1.m[2]/16384;
+            fx1 = (float) f10/100;
+            fy1 = (float) f11/100;
+            fz1 = (float) f12/100;
+            mx1 = (float) m10/100;
+            my1 = (float) m11/100;
+            mz1 = (float) m12/100;
+
+            /** ZMP Equations : Double Support **/
+            xzmp0 = -my0 / fz0;
+            yzmp0 = mx0 / fz0;
+
+            xzmp1 = -my1 /fz1;
+            yzmp1 = mx1 /fz1;
+
+            xzmp = (xzmp0 * fz0 + xzmp1 * fz1) / (fz0 + fz1);
+            yzmp = (yzmp0 * fz0 + yzmp1 * fz1) / (fz0 + fz1);
+
+            printf("%f , %f\n", fz0, fz1);
+            printf("ZMP = [ %f ,%f]\n", xzmp, yzmp);
+
+            //if ((fz0 == 0.0 ) || ( fz1 == 0.0)) {
+            if ((xzmp != xzmp) || (yzmp != yzmp)){
+                printf ("Warning: No zmp data\n");
+            } else {
+
+                /** EVALUACION MODELO **/
+                modelo.model(xzmp, yzmp);
+
+                posRightLeg->positionMove(4, modelo._u);
+                posLeftLeg->positionMove(4, modelo._u);
+
+                cout << "model ok" << endl;
+
+                //modelo.sendData(port1, data);
+
+                //            bottle.addDouble(modelo._x1);
+                //            bottle.addDouble(modelo._x2);
+                //            bottle.addDouble(modelo._u);
+                //            bottle.addDouble(modelo._y);
+                //            bottle.addDouble(xzmp);
+                //            bottle.addDouble(yzmp);
+
+                port.write(bottle);
+
+                fprintf(fp,"\n%d", n);
+                fprintf(fp,",%f", xzmp);
+                fprintf(fp,",%f", yzmp);
+                fprintf(fp,",%f", modelo._x_model[0]);
+                fprintf(fp,",%f", modelo._y_model[0]);
+                fprintf(fp,",%f", modelo._u);
+                fprintf(fp,",%f", modelo._par);
             }
-            for (int i=0;i<3;i++) {
-                int m = 10*fm.m[i]*fs.m[i]/16384; // Decimas de N·m
-                mx = 10*fm.m[0]*fs.m[0]/16384;
-                my = 10*fm.m[1]*fs.m[1]/16384;
-                printf("%d ",m);
-                bottle.addInt(m);
-            }
 
-            printf("]\n");
-
-            //printf("--\n");
-            //printf("%d\n",(short)fs.v[1]/16384);
-            //printf("%d\n",(short)fs.v[2]/16384);
-
-            fz = fz /100; // Newton
-            my = my /10; // Newton·metro
-            mx = mx/10; // N·m
+            n++;
 
 
-            // CALCULO ZMP
-            if (fz !=0.0){
-                xzmp = (float) (-my / fz);
-                yzmp = (float) (mx / fz);
-            } else{
-                xzmp=0;
-                yzmp=0;
-            }
-
-            cout << "zmp = [ " << xzmp << " , " << yzmp <<" ]" << endl;
-
-            /** EVALUACION MODELO
-          **/
-            modelo.model(xzmp, yzmp);
-            cout << "model ok" << endl;
-            //modelo.sendData(port1, data);
-
-            bottle.addDouble(modelo._x1[0]);
-            bottle.addDouble(modelo._x2[0]);
-            bottle.addDouble(modelo._u);
-            bottle.addDouble(modelo._y);
-
-            port.write(bottle);
-            cout << "Data sent"<< endl;
-
-            printf("Left Leg positionMove(0,0)\n");
-            posLeftLeg->positionMove(0, 0);
-            printf("Left Leg positionMove(0,0)\n");
-            posLeftLeg->positionMove(1, 0);
-            printf("Left Leg positionMove(0,0)\n");
-            posLeftLeg->positionMove(2, 0);
-            printf("Left Leg positionMove(0,0)\n");
-            posLeftLeg->positionMove(3, 0);
-            printf("Left Leg positionMove(0,0)\n");
-            posLeftLeg->positionMove(4, 0);
-            printf("Left Leg positionMove(0,0)\n");
-            posLeftLeg->positionMove(5, 0);
 
         } else perror("");
-
-
 
         //Sample time = 1ms
         usleep(100000); // delay in microseconds
         //sleep(1);
     }
+
+    fclose(fp);
     close(fd);
 }
 
