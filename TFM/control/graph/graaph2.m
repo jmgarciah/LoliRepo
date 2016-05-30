3d9080859935134e94a9229680d430f049eaf991
lpinel <lolipv92@hotmail.com>
2016-05-30 12:20:36 +0200

    Updated fig

--- TFM/control/graph/graaph2.m
+++ TFM/control/graph/graaph2.m
@@ -1,10 +1,10 @@
-data = csvread('/home/teo/Documentos/data.csv');
+data = csvread('/home/teo/Descargas/data.csv');
 n = data(:,1);
 t = data(:,2);
 zmp = data(:,3);
 y = data(:,4);
-x1 = data(:,5);
-Uref = data(:,6);
+zmp_error = data(:,5);
+zmp_ref = data(:,6);
 u = data(:,7);
 angle = data(:,8);
 
@@ -19,12 +19,12 @@ right = -(140+37.5)/1000*ones(size(n));
 hold on
 plot(t,zmp,'g')
 plot(t,y,'r')
-plot(t,x1,'b')
-plot(t,Uref,'k')
+plot(t,zmp_error,'b')
+plot(t,zmp_ref,'k')
 plot(t,u,'m')
 plot(t,angle,'y')
 plot(t,front,'k--',t,back,'k--');
 
 title('K=[12.55, 4.91], Ki=10, Kp=-0.7, Ku=-2.05')
 xlabel('t [s]');
-legend('zmp','y{out}','x1','Ud(rad)','u(rad)','vel(deg/s)')
+legend('zmp','y{out}','zmp_error','zmp_ref','u','angle')
