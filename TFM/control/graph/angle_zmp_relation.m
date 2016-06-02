bc2b16829175dea675fbc5b1b356382ea17973a2
lpinel <lolipv92@hotmail.com>
2016-06-02 21:41:30 +0200

    New tests added

--- TFM/control/graph/angle_zmp_relation.m
+++ TFM/control/graph/angle_zmp_relation.m
@@ -1,4 +1,4 @@
-% Data (measures in mm)
+%% Data (measures in mm) - 19/05/2016
 angle = [0 -0.5 -1 -1.5 -2 -2.5 -3 -3.5 -4 -4.5 -5];
 test1 = [-14.5; -9; -3.5; 0; 8.5; 17.5; 25; 33.5; 47.5; 53; 60]/1000;
 test2 = [-14.5; -8.6; -3.7; -2.5; 8.6; 17; 24.5; 32.5; 42.5; 51; 56.5]/1000;
@@ -23,3 +23,28 @@ title ('Angle - ZMP relation');
 xlabel('angle [deg]');
 ylabel('ZMP [m]');
 legend('test1','test2','test3','test4','test5', 'Average');
+
+
+%% Data (measures in mm) - 2/06/2016
+% Offset deleted at the beginning
+% angles in deg and zmp in metres
+angle = [0 -0.5 -1 -1.5 -2 -2.5 -3 -3.5 -4 -4.5 -5];
+test1 = [0 0.005 0.012 0.0175 0.024 0.0305 0.037 0.043 0.051 0.06 0.0665];
+test2 = [0 0.005 0.01 0.018 0.023 0.034 0.04 0.049 0.052 0.065 0.071]
+p = polyfit(angle, test1, 1);
+line = p(1)*angle + p(2);
+p2= polyfit(angle, test2, 1);
+line2 = p2(1)*angle + p2(2);
+
+figure; hold on; box on;
+plot(angle, test1, 'bo');
+plot(angle, test2, 'go');
+plot(angle, line,'r');
+plot(angle, line2,'r');
+xlabel('angle [deg]');
+ylabel('ZMP [mm]');
+gtext(['zmp = ',num2str(p(1)),'*angle +',num2str(p(2))])
+gtext(['zmp = ',num2str(p2(1)),'*angle +',num2str(p2(2))])
+legend('ZMP F/T Test1','ZMP F/T Test2');
+
+
--- TFM/control/graph/zmp_real_ideal_relation.m
+++ TFM/control/graph/zmp_real_ideal_relation.m
@@ -15,17 +15,18 @@ ylabel('ZMP F/T');
 legend('ZMP real', 'ZMP ideal')
 
 % Lines above have a linear error increment
-figure;
-error =  ref - (0.78*ref-0.0029);
+%figure;
+error =  ref - (0.76*ref-0.002);
 plot(ref, error)
 xlabel('ZMP ref')
 ylabel('error')
-% Error to angle
 
 % Relation between angle commanded (ZMP/L)*180/pi and ZMP read
 figure; hold on;
 %plot(angle_ref, ref, 'r');
 plot(angle_ref, zmp, 'b')
+xlabel('angle sent (deg)');
+ylabel('zmp read (mm)');
 
 
 %Comparation angle-ZMP
@@ -36,3 +37,51 @@ plot(angle_ref, zmp1,'r')
 xlabel('angle(deg)');
 ylabel('ZMP (m)');
 legend('no offset', 'with offset');
+
+
+%Aproximacion a 1/x
+cociente = error ./ ref;
+y = (0.004 ./ (1.5*ref)) + 0.23 ;
+figure; hold on
+plot(ref, cociente,'bo');
+plot(ref, y,'r-');
+legend('cociente', 'ajuste');
+gtext('y = (0.004/(1.5*ref)) + 0.23')
+xlabel('ref');
+ylabel('error/ref');
+
+ref = 0.02;
+theta = (ref/0.0156) + (0.004/(1.5*ref)+0.23)*ref/0.0156
+
+
+%Aproximacion a exponencial y = c*e^ax
+cociente = error ./ ref;
+p = polyfit(ref,log(cociente),1);
+a = p(1);
+c = exp(p(2));
+fprintf('exponente a= %2.3f\n',a);
+fprintf('coeficiente c = %3.3f\n',c);
+
+y =  c * exp(a*ref);
+
+y2 =  c * exp(-100*ref)+0.28;
+
+plot(ref,y,'r',ref,y2,'g',ref, cociente, 'bo')
+xlabel('ref');
+ylabel('error/ref');
+legend('y = 0.51 * e^-14.792*ref', 'y = (0.51 * e^-100*ref )+0.28' )
+
+ref = 0.07;
+o = -((ref/0.0135) + ((0.5186*exp(-100*ref))))
+
+ref= 0.01;
+o = -(ref/0.0156) - ((0.5186*exp(-14.79*ref)-12*ref))
+
+% Comparation between angle error and zmp error.
+error =  ref - (0.76*ref-0.002);
+angle_error = +(0.76*ref-0.002)/0.0156;
+
+plot(error, angle_error, 'bo')
+xlabel('zmp')
+ylabel('|theta|');
+
